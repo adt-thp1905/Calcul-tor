d2c4bd8ec6dc753bd9884e1f3feff7dd27273763
adt-thp1905 <aadityathapliyal03@gmail.com>
2026-06-10 12:04:54 +0530

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,4 +1,16 @@
-x = input("Enter number 1 :")
-y = input("Enter number 2 :")
+x = float(input("Enter number 1 :"))
+y = float(input("Enter number 2 :"))
 z = input("Enter the desired operation: ")
-print(x ,{z}, y)
+if z == "+"
+  print(x + y)
+elif z == "-"
+  print(x - y)
+elif z == "*"
+  print(x * y)
+elif z == "/"
+  if y != 0
+   print(x/y)
+  else
+   print("Cannot divide by zero")
+else 
+  print("Invalid operator")
